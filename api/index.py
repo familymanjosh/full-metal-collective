62c87d34388edb379baa49f5ea39288e68b960aa
+ <familymanjosh@gmail.com>
2023-06-21 11:33:20 -0700

    making todo list

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,15 +10,15 @@ todo_id_counter = 1
 
 @app.route("/api/todos", methods=["GET"])
 def get_all_todo_items():
-    return todos
+    return jsonify(todos)
 
 
 @app.route("/api/todos/<int:todo_id>", methods=["GET"])
 def get_todo_item(todo_id):
     todo = next((todo for todo in todos if todo["id"] == todo_id), None)
     if todo:
-        return todo
-    return {"error": "Todo item not found"}, 404
+        return jsonify(todo)
+    return jsonify({"error": "Todo item not found"}), 404
 
 
 @app.route("/api/todos", methods=["POST"])
@@ -26,7 +26,7 @@ def create_todo_item():
     data = request.get_json()
     title = data.get("title")
     if not title:
-        return {"error": "Title is required"}, 400
+        return jsonify({"error": "Title is required"}), 400
 
     global todo_id_counter
     todo = {
@@ -36,7 +36,7 @@ def create_todo_item():
     }
     todos.append(todo)
     todo_id_counter += 1
-    return todo, 201
+    return jsonify(todo), 201
 
 
 @app.route("/api/todos/<int:todo_id>", methods=["PATCH"])
@@ -51,21 +51,21 @@ def update_todo_item(todo_id):
             todo["title"] = title
         if completed is not None:
             todo["completed"] = completed
-        return todo
-    return {"error": "Todo item not found"}, 404
+        return jsonify(todo)
+    return jsonify({"error": "Todo item not found"}), 404
 
 
 @app.route("/api/todos/<int:todo_id>", methods=["DELETE"])
 def delete_todo_item(todo_id):
     global todos
     todos = [todo for todo in todos if todo["id"] != todo_id]
-    return {"message": "Todo item deleted"}
+    return jsonify({"message": "Todo item deleted"})
 
 
 @app.route("/api/healthchecker", methods=["GET"])
 def healthchecker():
-    return {"status": "success", "message": "Integrate Flask Framework with Next.js"}
+    return jsonify({"status": "success", "message": "Integrate Flask Framework with Next.js"})
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run()
